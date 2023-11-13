b7cb89cd9aaad7d25d1066f80ba7c0366215ac89
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2023-11-13 12:35:03 -0500

    Remove the override of ascds env vars to allow acorn to process FMT6 data lines correctly into tl tracelog files

--- Dump/run_filter_scripts.py
+++ Dump/run_filter_scripts.py
@@ -26,9 +26,6 @@ import unittest
 #
 from Ska.Shell import getenv, bash
 ascdsenv = getenv('source /home/ascds/.ascrc -r release; source /home/mta/bin/reset_param', shell='tcsh')
-ascdsenv['IPCL_DIR'] = "/home/ascds/DS.release/config/tp_template/P011/"
-ascdsenv['ACORN_GUI'] = "/home/ascds/DS.release/config/mta/acorn/scripts/"
-ascdsenv['LD_LIBRARY_PATH'] = "/home/ascds/DS.release/lib:/home/ascds/DS.release/ots/lib:/soft/SYBASE_OSRV15.5/OCS-15_0/lib:/home/ascds/DS.release/otslib:/opt/X11R6/lib:/usr/lib64/alliance/lib"
 #
 #--- read directory path
 #
