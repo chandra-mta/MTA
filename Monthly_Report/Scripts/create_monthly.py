4d7479343b234ea0be18a02428f53f7724314f4f
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2024-02-27 14:02:35 -0500

    update script set

--- Monthly_Report/Scripts/create_monthly.py
+++ Monthly_Report/Scripts/create_monthly.py
@@ -6,7 +6,7 @@
 #                                                                                       #
 #           author: t. isobe (tisobe@cfa.harvard.edu)                                   #
 #                                                                                       #
-#           last update: Mar 10, 2021                                                   #
+#           last update: Oct 07, 2022                                                   #
 #                                                                                       #
 #########################################################################################
 
@@ -46,6 +46,14 @@ sys.path.append(mta_dir)
 #--- converTimeFormat contains MTA time conversion routines
 #
 import mta_common_functions as mcf
+
+#
+#--- Recieve Admin email list from sys args
+#
+ADMIN = ['mtadude@cfa.harvard.edu']
+for i in range(1,len(sys.argv)):
+    if sys.argv[i][:6] == 'email=':
+        ADMIN.append(sys.argv[i][6:])
 
 #-----------------------------------------------------------------------------------
 #-- create_monthly: create monthly report                                         --
@@ -308,7 +316,9 @@ def create_monthly(year='', mon=''):
 #
 #--- create clean acis and hrc exposure maps using ds9
 #
-    run_exposure_maps(lyear, cmon)
+    #run_exposure_maps(lyear, cmon)
+
+    send_email_to_admin()
 
 #--------------------------------------------------
 #--- read the template and substitute the contents 
@@ -982,11 +992,43 @@ def create_exposure_map(fits):
     except:
         pass
 
+#----------------------------------------------------------------------------------
+#-- send_email_to_admin: send out a notification email to admin                  --
+#----------------------------------------------------------------------------------
+
+def send_email_to_admin():
+    """
+    send out a notification email to admin
+    input:  none
+    output: email to admin
+    """
+    out   = Chandra.Time.DateTime().date
+    atemp = re.split('\.', out)
+    ctime = time.strftime('%Y-%m', time.strptime(atemp[0], '%Y:%j:%H:%M:%S'))
+    btemp = re.split('-', ctime)
+    year  = btemp[0]
+    mon   = int(float(btemp[1]))
+    mon = mon - 1 #Pulls current month, report is for previous month.
+    if (mon == 0):#If current month is Jan, then monthly report records Dec of previous year.
+       year = int(float(year))
+       year = year - 1
+       mon = 12
+
+    mon   = mcf.change_month_format(mon).upper()
+
+    line = 'Monthly Report is created. Please create exposure maps and copy solar cycle plots.\n '
+    line = line + 'https://cxc.harvard.edu/mta/REPORTS/MONTHLY/'
+    line = line + str(year) + str(mon) + '/MONTHLY.html\n\n'
+    line = line + "Don't forget to edit index file: /data/mta4/www/REPORTS/MONTHLY/index.html.\n"
+    
+    cmd = f'echo "{line}" | mailx -s "Subject: Monthly Report for {str(mon)} Created" {" ".join(ADMIN)}'
+    os.system(cmd)
+
 #-----------------------------------------------------------------
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 3:
+    if (len(sys.argv) >= 3 and sys.argv[1][:6] != 'email=' and sys.argv[2][:6] != 'email='):
         year = sys.argv[1]
         year = int(float(year))
         mon  = sys.argv[2]
