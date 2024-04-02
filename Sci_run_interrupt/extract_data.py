214ae6e9ecc1331229928689a33f0868ea4fc11a
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2024-04-02 14:47:09 -0400

    format data extraction for event_data

--- Sci_run_interrupt/extract_data.py
+++ Sci_run_interrupt/extract_data.py
@@ -10,33 +10,19 @@
 #                                                                               #
 #################################################################################
 
-import math
 import re
 import sys
-import os
-import string
-#
-#--- reading directory list
-#
 
-path = '/data/mta/Script/Interrupt/Scripts/house_keeping/dir_list'
-with open(path, 'r') as f:
-    data = [line.strip() for line in f.readlines()]
+#
+#--- Define Directory Pathing
+#
+BIN_DIR = '/data/mta/Script/Interrupt/Scripts'
 
-for ent in data:
-    atemp = re.split(':', ent)
-    var  = atemp[1].strip()
-    line = atemp[0].strip()
-    exec("%s = %s" %(var, line))
 #
 #--- append a path to a privte folder to python directory
 #
-sys.path.append(bin_dir)
-sys.path.append(mta_dir)
-#
-#--- mta common functions
-#
-import mta_common_functions             as mcf
+sys.path.append(BIN_DIR)
+
 #
 #--- Science Run Interrupt related funcions shared
 #
@@ -70,79 +56,59 @@ import sci_run_add_to_rad_zone_list     as rzl
 #--- extract_data: extract ephin and GOES data. this is a control and call a few related scripts
 #-------------------------------------------------------------------------------------
 
-def extract_data(ifile):
+def extract_data(event_data):
     """
     extract ephin and GOES data. this is a control and call a few related scripts 
-    input: ifile    --- a file contain the data information
-                        e.g., 20170911    2017:09:11:07:51    2017:09:13:22:56    171.6   auto
+    input: event_data --- dictionary containing event data
+        e.g., name: 20170911    tstart: 2017:09:11:07:51    tstop: 2017:09:13:22:56    tlost: 171.6   mode: auto
     output: all data files and stat data file for the event(s)
     """
-    if ifile == '':
-        ifile = input('Please put the intrrupt timing list: ')
 
     rzl.sci_run_add_to_rad_zone_list(ifile)
-#
-#--- correct science run interruption time excluding radiation zones
-#
-    itrf.sci_run_compute_gap(ifile)
-
-    data = mcf.read_data_file(ifile)
-
-    for ent in data:
-        print( "EXTRACTING DATA FOR: " + ent)
-
-        if not ent:
-            break
-
-        atemp = re.split('\s+|\t+', ent)
-        event = atemp[0]
-        start = atemp[1]
-        stop  = atemp[2]
-        gap   = atemp[3]
-        itype = atemp[4]
+#event = name
 #
 #--- extract ephin/hrc data
 #
-        ephin.ephin_data_extract(event, start, stop)
+    ephin.ephin_data_extract(event, start, stop)
 #
 #--- compute ephin/hrc statistics
 #
-        ephin.compute_ephin_stat(event, start)
+    ephin.compute_ephin_stat(event, start)
 #
 #---- extract GOES data
 #
-        try:
-            goes.extract_goes_data(event, start, stop)
-        except:
-            pass
+    try:
+        goes.extract_goes_data(event, start, stop)
+    except:
+        pass
 #
 #---- compute GOES statistics
 #
-        try:
-            goes.compute_goes_stat(event, start)
-        except:
-            pass
+    try:
+        goes.compute_goes_stat(event, start)
+    except:
+        pass
 #
 #---- extract ACE (NOAA) data
 #
-        try:
-            ace.extract_ace_data(event, start, stop)
-        except:
-            pass
+    try:
+        ace.extract_ace_data(event, start, stop)
+    except:
+        pass
 #
 #---- compute ACE statistics
 #
-        try:
-            astat.compute_ace_stat(event, start, stop)
-        except:
-            pass
+    try:
+        astat.compute_ace_stat(event, start, stop)
+    except:
+        pass
 #
 #---- extract/compute/plot xmm data
 #
-        try:
-            xmm.read_xmm_and_process(event)
-        except:
-            pass
+    try:
+        xmm.read_xmm_and_process(event)
+    except:
+        pass
 
 #-----------------------------------------------------------------------------------------
 
